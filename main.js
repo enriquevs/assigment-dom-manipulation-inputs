4501f8f405e07ba117884463c03bc7d9d03869c8
Luis Enrique Vargas Sánchez <luisenrique@MacBook-Pro-de-Luis.local>
2018-12-09 17:30:14 -0600

    final commit

--- main.js
+++ main.js
@@ -59,4 +59,53 @@ document.querySelector("#calculate-items-btn").addEventListener('click', functio
 
 document.querySelector("#select-to-show-more-btn").addEventListener('click', function(){
   // TASK #4
-})
+  var imgContainer = document.querySelector('.img-box')
+    var data = document.querySelector('.selection')
+    var model = document.querySelector('.model')
+    var msrp = document.querySelector('.msrp')
+    var mpg = document.querySelector('.mpg')
+    var edmund = document.querySelector('.edmunds')
+    var optionArray = document.querySelectorAll('option')
+
+
+    for (var i = 0; i < optionArray.length; i++) {
+
+      if(data.selectedIndex === 0 && optionArray[i].value === "Mini Cooper"){
+
+        imgContainer.innerHTML = "<img src='images/"+ optionArray[i].dataset.img + "'></img>"
+
+        model.textContent = optionArray[i].value
+
+        msrp.textContent = optionArray[i].dataset.msrp
+
+        mpg.textContent = optionArray[i].dataset.mpg
+
+        edmund.textContent = optionArray[i].dataset.edmunds
+
+      } else if (data.selectedIndex == 1 && optionArray[i].value === "Mazda 3" )  {
+
+        imgContainer.innerHTML = "<img src='images/"+ optionArray[i].dataset.img + "'></img>"
+
+        model.textContent = optionArray[i].value
+
+        msrp.textContent = optionArray[i].dataset.msrp
+
+        mpg.textContent = optionArray[i].dataset.mpg
+
+        edmund.textContent = optionArray[i].dataset.edmunds
+
+      }else if (data.selectedIndex == 2 && optionArray[i].value === "Dodge Charger") {
+
+        imgContainer.innerHTML = "<img src='images/"+ optionArray[i].dataset.img + "'></img>"
+
+        model.textContent = optionArray[i].value
+
+        msrp.textContent = optionArray[i].dataset.msrp
+
+        mpg.textContent = optionArray[i].dataset.mpg
+
+        edmund.textContent = optionArray[i].dataset.edmunds
+      }
+    }
+
+  })
